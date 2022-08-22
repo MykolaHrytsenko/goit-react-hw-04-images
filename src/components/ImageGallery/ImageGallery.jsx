ebdb75bbcd2983dc6c1394102192ca1edf997ecb
Mykola_Hrytsenko <94016020+MykolaHrytsenko@users.noreply.github.com>
2022-08-22 14:05:55 +0300

    fix

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,17 +1,22 @@
 import PropTypes from 'prop-types';
-import { GalleryList } from "./ImageGallery.styled";
-import { ImageGalleryItem } from "components/ImageGalleryItem/ImageGalleryItem";
+import { GalleryList } from './ImageGallery.styled';
+import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
-export const ImageGallery = ({items, onClick}) => {
-    return (
-        <GalleryList>
-            {items.map(item => {
-                return <ImageGalleryItem key={item.id} item={item} onClick={onClick}/>})}
-        </GalleryList>
-    )
+export const ImageGallery = ({ items, onClick }) => {
+  return (
+    <GalleryList>
+      {items.map(item => {
+        return <ImageGalleryItem key={item.id} item={item} onClick={onClick} />;
+      })}
+    </GalleryList>
+  );
 };
 
 ImageGallery.propTypes = {
-    items: PropTypes.arrayOf(PropTypes.object),
-    onClick: PropTypes.func.isRequired,
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+    })
+  ),
+  onClick: PropTypes.func.isRequired,
 };
